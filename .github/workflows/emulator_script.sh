5dd04b263368b0e4532b94256e08a593a9e61bc4
Stephan Petzl <spetzl@gmx.at>
2022-10-11 17:23:09 +0200

    Update emulator_script.sh

--- .github/workflows/emulator_script.sh
+++ .github/workflows/emulator_script.sh
@@ -3,8 +3,8 @@ adb devices
 
 # Checking keys and secrets are set properly
 echo "Trick to echo GitHub Actions Secret:"
-echo $AWS_BUCKET
-echo $LICENSE_KEY | sed 's/./& /g' 
+# echo $AWS_BUCKET
+# echo $LICENSE_KEY | sed 's/./& /g' 
 
 #flutter build apk --split-per-abi # to make build of app but we are using github action for build
 
@@ -23,7 +23,7 @@ MACHINE_TYPE=`uname -m`
 
 # Use below script to install latest CLI always
 # RELEASE_URL=$(node ./.github/workflows/latest-cli-release.js "${MACHINE_TYPE}" "") 
-RELEASE_URL="https://github.com/repeato-qa/Repeato-CLI-prebuilt/releases/download/v1.2.3/repeato-cli-mac.zip"
+RELEASE_URL="https://github.com/repeato-qa/Repeato-CLI-prebuilt/releases/download/v1.2.4/repeato-cli-mac.zip"
 wget -q "${RELEASE_URL}" -O repeato-cli.zip
 unzip -qq repeato-cli.zip -d repeato-cli/
 
