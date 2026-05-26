9d373e732b8da39a5eb958e96089009c9f897822
Stephan Petzl <stephan.petzl@gmail.com>
2026-05-26 12:12:48 +0200

    Fix Flutter theme API usage

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -28,18 +28,24 @@ import 'package:flutter/material.dart';
 final ThemeData themeData = new ThemeData(
     fontFamily: FontFamily.productSans,
     brightness: Brightness.light,
-    primarySwatch: MaterialColor(AppColors.orange[500]!.value, AppColors.orange),
+    primarySwatch:
+        MaterialColor(AppColors.orange[500]!.value, AppColors.orange),
     primaryColor: AppColors.orange[500],
-    primaryColorBrightness: Brightness.light,
     accentColor: AppColors.orange[500],
-    accentColorBrightness: Brightness.light
-);
+    colorScheme: ColorScheme.fromSwatch(
+      primarySwatch:
+          MaterialColor(AppColors.orange[500]!.value, AppColors.orange),
+      brightness: Brightness.light,
+    ).copyWith(secondary: AppColors.orange[500]));
 
 final ThemeData themeDataDark = ThemeData(
   fontFamily: FontFamily.productSans,
   brightness: Brightness.dark,
   primaryColor: AppColors.orange[500],
-  primaryColorBrightness: Brightness.dark,
   accentColor: AppColors.orange[500],
-  accentColorBrightness: Brightness.dark,
+  colorScheme: ColorScheme.fromSwatch(
+    primarySwatch:
+        MaterialColor(AppColors.orange[500]!.value, AppColors.orange),
+    brightness: Brightness.dark,
+  ).copyWith(secondary: AppColors.orange[500]),
 );
